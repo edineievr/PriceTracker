69c1f899d5e79e285a0d2b68a733b1006acfb9e0
edineievr <edney2322@gmail.com>
2026-04-28 19:37:10 -0300

    implementa alerta de preço via telegram

--- Services/NotificationService.cs
+++ Services/NotificationService.cs
@@ -1,6 +1,34 @@
-﻿namespace PriceTracker.Services
+﻿using PriceTracker.DTOs;
+using static System.Net.WebRequestMethods;
+
+namespace PriceTracker.Services
 {
     public class NotificationService
     {
+        private readonly string _apiKey;
+        private readonly string _chatId;
+
+        public NotificationService()
+        {
+            DotNetEnv.Env.Load();
+            _apiKey = Environment.GetEnvironmentVariable("TELEGRAM_API_KEY");
+            _chatId = Environment.GetEnvironmentVariable("TELEGRAM_CHAT_ID");
+        }
+        public async Task SendNotification(PriceAlertDto priceAlert)
+        {
+
+            var httpClient = new HttpClient();            
+
+            var payload = new
+            {
+                chat_id = _chatId,
+                text = $"Alerta de Preço: {priceAlert.ProductDescription}\nPreço Atual: {priceAlert.CurrentPrice}\nPlataforma: {priceAlert.Platform}",
+                parse_mode = "Markdown"
+            };
+
+            var response = await httpClient.PostAsJsonAsync($"https://api.telegram.org/bot{_apiKey}/sendMessage",payload);
+
+            Console.WriteLine(await response.Content.ReadAsStringAsync());//nao faço ideia do que isso ta fazendo
+        }
     }
 }
