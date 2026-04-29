c9ab4c69224988737bc14cda115e05c382ee8568
edineievr <edney2322@gmail.com>
2026-04-29 20:31:20 -0300

    cria factory para resolver qual estrategia utilizar de acordo com a plataforma

---
+++ PriceTrackerFactory.cs
@@ -0,0 +1,23 @@
+﻿using PriceTracker.Intefaces;
+using PriceTracker.Strategies;
+
+namespace PriceTracker
+{
+    public class PriceTrackerFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public PriceTrackerFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IPriceScraper CreatePriceTracker(string platform)
+        {
+            return platform switch
+            {
+                "Mercado Livre" => _serviceProvider.GetRequiredService<MeliStrategy>(),
+                _ => throw new NotSupportedException($"Platform '{platform}' is not supported.")
+            };
+        }
+    }
+}
