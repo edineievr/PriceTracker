31f0600ede47c698bd080e5d9c20b1a2f33ac2d9
edineievr <edney2322@gmail.com>
2026-04-29 20:30:50 -0300

    documenta o proximo passo a ser feito no comparador de preço

--- Services/PriceComparisonService.cs
+++ Services/PriceComparisonService.cs
@@ -1,4 +1,5 @@
-﻿using PriceTracker.Intefaces;
+﻿using PriceTracker.DTOs;
+using PriceTracker.Intefaces;
 using PriceTracker.Models;
 
 namespace PriceTracker.Services
@@ -7,15 +8,23 @@ namespace PriceTracker.Services
     {
         private readonly IPriceScraper _priceScraper;
         private readonly NotificationService _notificationService;
+        private readonly PriceTrackerFactory _priceTrackerFactory;
 
-        public PriceComparisonService(IPriceScraper priceScraper, NotificationService notificationService)
+        public PriceComparisonService(IPriceScraper priceScraper, NotificationService notificationService, PriceTrackerFactory priceTrackerFactory)
         {
             _priceScraper = priceScraper;
             _notificationService = notificationService;
+            _priceTrackerFactory = priceTrackerFactory;
         }
 
         public async void PriceComparison(string url)
-        {            
+        {
+            //buscar no banco os produtos a serem monitorados
+
+            //o prduto conterá a respectiva plataforma
+
+            //aqui eu resolvo qual estrategia de extração de preço a ser utilizada, de acordo com a plataforma do produto
+
             var result = await _priceScraper.ExtractPrice(url) ?? throw new Exception("Falha ao extrair informações da url informada");
 
             var historico = new PriceHistory
@@ -23,13 +32,26 @@ namespace PriceTracker.Services
                 ProductName = result.ProductDescription,
                 Platform = result.Platform,
                 Link = url,
-                CurrentPrice = result.Price
+                CurrentPrice = result.CurrentPrice
             };
 
-            if (historico.PriceChanged())
+
+            if (!string.IsNullOrWhiteSpace(historico.OldPrice))
             {
-                //_notificationService.SendNotification(historico);
-            }
+                if (historico.PriceChanged())
+                {
+                    var priceAlert = new PriceAlertDto
+                    {
+                        CurrentPrice = historico.CurrentPrice,
+                        OldPrice = historico.OldPrice,
+                        ProductDescription = historico.ProductName
+                    };
+
+                    await _notificationService.SendNotification(priceAlert);
+                }
+            }            
+
+            //persistir o histórico de preços no banco de dados independente de ter ou não alteração de preço
         }
     }
 }
