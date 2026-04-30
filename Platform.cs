1b0860098969f5b4c6583cdb57eda3c67b3b01df
edineievr <edney2322@gmail.com>
2026-04-30 20:19:07 -0300

    cria enum para melhor controle das plataformas de tracking de preço

---
+++ Platform.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace PriceTracker
+{
+    public enum Platform
+    {
+        [Description("Mercado Livre")]
+        Meli = 0,
+
+        [Description("Kabum")]
+        Kabum = 1,
+        // Adicione outras plataformas conforme necessário
+    }
+}
