2c080b63fe02b1777a099d24d9adc5dd18663742
edineievr <edney2322@gmail.com>
2026-04-30 20:15:10 -0300

    implementa classe para abertura de conexão com sqlite

---
+++ Infrastructure/Database.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace PriceTracker.Infrastructure
+{
+    public class Database
+    {
+        private readonly string _connectionString;
+
+        public Database(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Initialize()
+        {
+            using var connection = new SqliteConnection(_connectionString);
+
+            connection.Open();
+
+            var command = connection.CreateCommand();
+
+            command.CommandText = """                
+                 CREATE TABLE IF NOT EXISTS PriceHistory                 
+                 (Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 ProductDescription  TEXT NOT NULL,
+                 Platform            TEXT NOT NULL,
+                 Url                 TEXT NOT NULL,
+                 Price               REAL NOT NULL,
+                 RecordedAt          TEXT NOT NULL)                
+                """;
+            command.ExecuteNonQuery();
+        }
+    }
+}
