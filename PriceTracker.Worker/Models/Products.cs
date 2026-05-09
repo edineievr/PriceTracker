e8630fbd15ba39bbcc188f5272b5ad04d10a97fd
edineievr <edney2322@gmail.com>
2026-05-09 10:46:23 -0300

    refactor: implementa metodo de criação de produto na propria model para separacao de responsabilidade

--- PriceTracker.Worker/Models/Products.cs
+++ PriceTracker.Worker/Models/Products.cs
@@ -4,9 +4,21 @@ namespace PriceTracker.Worker.Models
 {
     public class Product
     {
-        public int Id { get; set; }
-        public string Description { get; set; }
-        public Platform Platform { get; set; }
-        public string Url { get; set; }
+        public int Id { get; protected set; }
+        public string Description { get; private set; }
+        public Platform Platform { get; private set; }
+        public string Url { get; private set; }
+        public bool IsActive { get; private set; }//criado apenas para desativar uma estrategia temporariamente (meli), mas será implementado na automação da alimentação do banco
+
+        public static Product Create(int id, string description, Platform platform, string url)
+        {
+            return new Product
+            {
+                Id = id,
+                Description = description,
+                Platform = platform,
+                Url = url,
+            };
+        }
     }
 }
