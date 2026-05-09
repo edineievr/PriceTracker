9f890e94dfc10733ca06cf4449e9eb7ecdd2917f
edineievr <edney2322@gmail.com>
2026-05-09 10:52:54 -0300

    chore: desativa temporariamente 'MeliStrategy' devido a mudanças na renderização de html

--- PriceTracker.Worker/Strategies/MeliStrategy.cs
+++ PriceTracker.Worker/Strategies/MeliStrategy.cs
@@ -1,62 +1,68 @@
-﻿using AngleSharp;
-using PriceTracker.Worker.DTOs;
-using PriceTracker.Worker.Enums;
-using PriceTracker.Worker.Intefaces;
-using System.Globalization;
+﻿//using AngleSharp;
+//using PriceTracker.Worker.DTOs;
+//using PriceTracker.Worker.Enums;
+//using PriceTracker.Worker.Intefaces;
+//using System.Globalization;
 
-namespace PriceTracker.Worker.Strategies
-{
-    public class MeliStrategy : IPriceScraper
-    {
-        public async Task<ProductTrackingResult> ExtractPriceAsync(string url)
-        {
-            try
-            {
-                var config = Configuration.Default.WithDefaultLoader();
-                var context = BrowsingContext.New(config);
-                var document = await context.OpenAsync(url);
+//namespace PriceTracker.Worker.Strategies
+//{
+//    public class MeliStrategy : IPriceScraper
+//    {
+//        private readonly ILogger<MeliStrategy> _logger;
 
-                var result = new ProductTrackingResult
-                {
-                    Platform = Platform.Meli,
-                };                
+//        public MeliStrategy(ILogger<MeliStrategy> logger)
+//        {
+//            _logger = logger;
+//        }
+
+//        public async Task<ProductTrackingResult> ExtractPriceAsync(string url)//temporariamente desativado, pois o site mudou a estrutura e precisa ser refeito o método de extração
+//        {
+//            try
+//            {
+//                var config = Configuration.Default.WithDefaultLoader();
+//                var context = BrowsingContext.New(config);
+//                var document = await context.OpenAsync(url);
+
+//                var result = new ProductTrackingResult
+//                {
+//                    Platform = Platform.Meli,
+//                };                
                 
-                var titleElement = document.QuerySelector("h1.ui-pdp-title");//extrai o nome do produto atraves do titulo no site do meli
+//                var titleElement = document.QuerySelector("h1.ui-pdp-title");
                 
-                if (titleElement != null)
-                {
-                    result.ProductDescription = titleElement.TextContent.Trim();
-                }
-                else
-                {
-                    result.ProductDescription = "Não foi possível extrair o nome, verificar metodo de extração";
-                }                
+//                if (titleElement != null)
+//                {
+//                    result.ProductDescription = titleElement.TextContent.Trim();
+//                }
+//                else
+//                {
+//                    result.ProductDescription = "Não foi possível extrair o nome, verificar metodo de extração";
+//                }                
                 
-                var priceContainer = document.QuerySelector("div.ui-pdp-price__second-line");//extrai o preço do produto atraves do container onde o preço é exibido no site do meli
+//                var priceContainer = document.QuerySelector("div.ui-pdp-price__second-line");
 
-                if (priceContainer != null)
-                {                    
-                    var fractionElement = priceContainer.QuerySelector("span[data-andes-money-amount-fraction]");
-                    var centsElement = priceContainer.QuerySelector("span[data-andes-money-amount-cents]");
+//                if (priceContainer != null)
+//                {                    
+//                    var fractionElement = priceContainer.QuerySelector("span[data-andes-money-amount-fraction]");
+//                    var centsElement = priceContainer.QuerySelector("span[data-andes-money-amount-cents]");
 
-                    if (fractionElement != null && centsElement != null)
-                    {
-                        var fraction = fractionElement.TextContent.Trim();//pega numero à esquerda da vírgula
-                        var cents = centsElement.TextContent.Trim(); //pega numero à direita da vírgula  
+//                    if (fractionElement != null && centsElement != null)
+//                    {
+//                        var fraction = fractionElement.TextContent.Trim();
+//                        var cents = centsElement.TextContent.Trim();
+//                        var priceString = $"{fraction},{cents}";
+//                        result.Price = decimal.Parse(priceString, CultureInfo.InvariantCulture);
+//                    }                  
+//                }
 
-                        var priceString = $"{fraction},{cents}";
-                        result.Price = decimal.Parse(priceString, CultureInfo.InvariantCulture);
-                    }                  
-                }
-
-                return result;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Erro ao extrair preço do Mercado Livre: {ex.Message}");//mudar isso aqui pra logar o erro em um arquivo de log ou algo do tipo
-                throw;
-            }
-        }
-    }
-}
+//                return result;
+//            }
+//            catch (Exception ex)
+//            {
+//                _logger.LogError(ex, "Erro ao extrair preço do produto no Meli. URL: {Url}", url);
+//                throw;
+//            }
+//        }
+//    }
+//}
 
