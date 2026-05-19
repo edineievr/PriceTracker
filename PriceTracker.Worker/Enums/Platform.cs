dfd07c7988300d4c62da57086297a5f148c8c8da
edineievr <edney2322@gmail.com>
2026-05-19 20:49:08 -0300

    remove Meli da lista de estrategias

--- PriceTracker.Worker/Enums/Platform.cs
+++ PriceTracker.Worker/Enums/Platform.cs
@@ -4,14 +4,13 @@ namespace PriceTracker.Worker.Enums
 {
     public enum Platform
     {
-        [Description("Mercado Livre")]
-        Meli = 0,
-
         [Description("Kabum")]
         Kabum = 1,
 
         [Description("Pichau")]
         Pichau = 2,
+
+        [Description("Terabyte")]
         Terabyte = 3,
         // Adicione outras plataformas conforme necessário
     }
